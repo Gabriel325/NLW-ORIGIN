0b8498e7554aecc42f83dfd0f96cd5f5ffc1a5fc
GabrielGazale <gazalegabriel.gag@gmail.com>
2021-11-02 13:24:35 -0300

    Menu funcionando

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-alert('Eu existo')
+/* Abrir e fechar o menu pelo icone: hamburger e x*/
+const nav = document.querySelector('#header nav')
+const toggles = document.querySelectorAll('nav .toggle')
+
+for (const element of toggles) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* Quando clicar em um item do menu, fechar menu*/
+const links = document.querySelectorAll('nav ul li a')
+for (var link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
