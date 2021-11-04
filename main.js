64e5c21b95b549ca3e08c19e4b11e8237b4e8916
GabrielGazale <gazalegabriel.gag@gmail.com>
2021-11-03 21:09:45 -0300

    Pagina Animada + Contatos feitos

--- main.js
+++ main.js
@@ -29,3 +29,28 @@ window.addEventListener('scroll', function () {
     header.classList.remove('scroll')
   }
 })
+
+/* Testimonials slider */
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+/* Scrollreveal: Carregar elementos quando der scroll na pagina */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  '#home .image, #home .text, #about .image, #about .text, #services header, #services .card, #testimonials header, #testimonials .testimonials, #contact .text, #contact .links',
+  {
+    interval: 100
+  }
+)
